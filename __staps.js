f7ae9b2f0076e16c2388df085b26ee1311c07728
SyedAlamin <syedalamin2580@gmail.com>
2023-04-24 23:16:58 +0600

    github signin add

--- __staps.js
+++ __staps.js
@@ -1,16 +1,31 @@
 /**
+ * --------------------
+ * INITIAL SETUP 
+ * --------------------
  * 1 visit: console.firebase.google.com 
  * 2. create project ( skip google analytics )
  * 3. register app ( create config )
  * 4. install firebase: npm install firebase
  * 5. add config file to your project 
  * 6. DANGER: Do not publish or make firebase config to public by pushing those to github
+ * -----------------
+ * INTEGRATION
+ * -----------------
  * 7. Visite: Go To Docs > Build > Authentication > Web > Get Started
  * 8. export app from the firebase.config.js file: export default app.
  * 9. Login.jsx: import getAuth from firebase/auth 
  * 10. create auth = getAuth(app)
+ * -----------------
+ * PROVIDER SETUP
+ * -----------------
  * 11. import googleAuthProvider and create new a provider
  * 12. use signInWithPopup and pass auth and provider
  * 13. activate sign-in mathod (google, facebook, github , etc)
  * 14. [vite] : change 127.0.0.1 to localhost
+ * -------------------
+ * MORE AUTH PROVIDER
+ * -------------------
+ * 1. actived the auth provider (create app, provide redirect, clint id, clint secret)
+ * 
+ * 
 */
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
 import app from '../../firebase/firebase.inti'
 const Login = () => {
     const auth = getAuth(app)
-    const provider = new GoogleAuthProvider();
+    const GoogleProvider = new GoogleAuthProvider();
+    const GitHubProvider = new GithubAuthProvider();
 
     const [user, setUser] = useState(null);
 
     const handleGoogleSignIn = () => {
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, GoogleProvider)
             .then(result => {
                 const loggedInUser = result.user;
                 console.log(loggedInUser)
@@ -18,8 +19,20 @@ const Login = () => {
                 console.log(error.message)
             })
     }
+    const handleGitHubSignIn = () => {
+        signInWithPopup(auth, GitHubProvider)
+            .then(result => {
+                const loggedInUser = result.user;
+                console.log(loggedInUser)
+                setUser(loggedInUser)
+            })
+            .catch(error =>{
+                console.log(error.message)
+            })
+    }
 
-    const handleGoogleSignOut = () => {
+
+    const handleSignOut = () => {
         signOut(auth)
             .then(result => {
                 console.log(result)
@@ -30,17 +43,22 @@ const Login = () => {
             })
     }
 
+
+
     return (
         <div>
             {
                 user ?
-                    <button onClick={handleGoogleSignOut}>Sign Out</button> :
-                    <button onClick={handleGoogleSignIn}>Google Login</button>
+                    <button onClick={handleSignOut}>Sign Out</button> :
+                    <div>
+                        <button onClick={handleGoogleSignIn}>Google Login</button>
+                        <button onClick={handleGitHubSignIn}>GitHub Login</button>
+                    </div>
             }
             {user && <div>
                 <h2>User : {user?.displayName}</h2>
                 <h4>Email : {user?.email}</h4>
-                <img src={user.photoURL} alt="" />
+                <img style={{ borderRadius: 50 }} src={user.photoURL} alt="" />
             </div>}
         </div>
     );
